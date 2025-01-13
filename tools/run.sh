d36f585e3b88d6073bee884ab947778de34a36c2
Roger Lee <lijintao@mail.ustc.edu.cn>
2025-01-14 01:05:33 +0800

    update manifest

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,3 @@
-clang++ -std=c++17 -o CIGSEGY CIGSEGY.cpp ../cigsegy/cpp/segyrw.cpp -I../cigsegy/cpp
+clang++ -std=c++17 -O3 -o CIGSEGY CIGSEGY.cpp ../cigsegy/cpp/segyrw.cpp -I../cigsegy/cpp
 # C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe
 # cl /EHsc /std:c++17 /utf-8 /O2 /I"tools" /I"cigsegy\cpp" tools\CIGSEGY.cpp cigsegy\cpp\segyrw.cpp /Fe:CIGSEGY.exe
